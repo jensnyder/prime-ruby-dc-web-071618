56180cadf75b8017724f29d8ac8917ebed926512
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-20 13:06:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+   if number > 1
+      (2..number-1).to_a.each do |i| 
+        if number % i == 0
+          return false
+        end
+      end
+    return true
+    else
+      return false
+    end
+end
